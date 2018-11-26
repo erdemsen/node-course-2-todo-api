48de02d0f01c45585824d77e219b059d15279179
erdem şen <erde_msn@windowslive.com>
2018-11-26 18:16:18 +0300

    Tests

---
+++ server/tests/server.test.js
@@ -0,0 +1,37 @@
+const expect=require('expect');
+const request = require('supertest');
+
+const {app}=require('./../server');
+const {ToDo}=require('./../models/todo');
+
+//testler yapılırken database in tamamen boşaltılmasını sağlar
+// beforeEach((done)=>{
+//   ToDo.remove({}).then(()=>{
+//     done();
+//   });
+// });
+
+describe('POST /todos',()=>{
+it('should create a new todo',(done)=>{
+  var text='Todo test';
+  request(app)
+  .post('/todos')
+  .send({
+    text
+  })
+  .expect(200)
+  .expect((res)=>{
+    expect(res.body.text).toBe(text);
+  })
+  .end((err,res)=>{
+    if(err){
+      return done(err);
+    }
+    // ToDo.find().then((todos)=>{
+    //
+    // });
+    done();
+  });
+});
+
+});
