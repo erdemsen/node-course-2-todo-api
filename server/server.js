dc802ddf2f6c8c021d8efd0f4aa0e95b0ec13c80
erdem şen <erde_msn@windowslive.com>
2018-11-27 01:29:05 +0300

    query samples add

--- server/server.js
+++ server/server.js
@@ -1,27 +1,64 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID}=require('mongodb');
 
-var {mongoose} = require('./db/mongoose.js');
-var {ToDo} = require('./models/todo');
-var {User} = require('./models/user');
+var {
+  mongoose
+} = require('./db/mongoose.js');
+var {
+  ToDo
+} = require('./models/todo');
+var {
+  User
+} = require('./models/user');
 
 var app = express();
 app.use(bodyParser.json());
 
-app.post('/todos',(req,res)=>{
+// A POST request for creating new todo
+app.post('/todos', (req, res) => {
   var todo = new ToDo({
-    text:req.body.text
+    text: req.body.text
   });
   //todo is a mongoose model so we can use .save method
-  todo.save().then((doc)=>{
+  todo.save().then((doc) => {
     res.send(doc);
-  },(e)=>{
+  }, (e) => {
     res.send(e);
   });
 });
 
-app.listen(3000,()=>{
+// GET request for getting all todos
+// app.get('/getTodos', (req, res) => {
+//   ToDo.find({}).then((doc) => {
+//     res.send(doc);
+//   }, (err) => {
+//     res.send(e);
+//   });
+// });
+
+// GET request for a special ToDo
+app.get('/getTodos/:id', (req, res) => {
+  var id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    res.status(404).send('ID is not valid');
+  } else {
+    ToDo.findById(id).then((doc) => {
+      if (doc)
+        res.send(doc);
+      else {
+        res.status(404).send();
+      }
+    }, (err) => {
+      res.status(400).send(e);
+    });
+  }
+});
+
+app.listen(3000, () => {
   console.log('Started on port 3000.');
 });
 
-module.exports={app};
+module.exports = {
+  app
+};
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,8 +1,12 @@
-const expect=require('expect');
+const expect = require('expect');
 const request = require('supertest');
 
-const {app}=require('./../server');
-const {ToDo}=require('./../models/todo');
+const {
+  app
+} = require('./../server');
+const {
+  ToDo
+} = require('./../models/todo');
 
 //testler yapılırken database in tamamen boşaltılmasını sağlar
 // beforeEach((done)=>{
@@ -11,27 +15,26 @@ const {ToDo}=require('./../models/todo');
 //   });
 // });
 
-describe('POST /todos',()=>{
-it('should create a new todo',(done)=>{
-  var text='Todo test';
-  request(app)
-  .post('/todos')
-  .send({
-    text
-  })
-  .expect(200)
-  .expect((res)=>{
-    expect(res.body.text).toBe(text);
-  })
-  .end((err,res)=>{
-    if(err){
-      return done(err);
-    }
-    // ToDo.find().then((todos)=>{
-    //
-    // });
-    done();
+describe('POST /todos', () => {
+  it('should create a new todo', (done) => {
+    var text = 'Todo test';
+    request(app)
+      .post('/todos')
+      .send({
+        text
+      })
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.text).toBe(text);
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+        // ToDo.find().then((todos)=>{
+        //
+        // });
+        done();
+      });
   });
-});
-
 });
